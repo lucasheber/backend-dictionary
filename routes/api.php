2b4be4476d884f096e65926eb84d0e4228c77725
Lucas Heber <lucas.heber07@gmail.com>
2025-04-24 15:59:14 -0300

    feat(dictionary): add routes for dictionary entries with authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DictionaryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +10,12 @@ use Illuminate\Support\Facades\Route;
 | Unauthenticated Routes
 |--------------------------------------------------------------------------
 */
+
 Route::get('/', function () {
     return response()->json(['message' => 'Fullstack Challenge 🏅 - Dictionary']);
 })->name('auth.index');
 
-Route::get('/login', fn () => response()->json(['message' => 'Login']))->name('login');
+Route::get('/login', fn() => response()->json(['message' => 'Login']))->name('login');
 Route::post('/auth/signup', [AuthController::class, 'signup']);
 Route::post('/auth/signin', [AuthController::class, 'signin']);
 
@@ -23,3 +25,13 @@ Route::post('/auth/signin', [AuthController::class, 'signin']);
 |--------------------------------------------------------------------------
 */
 Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
+
+// grouping routes with the auth:sanctum middleware
+Route::controller(DictionaryController::class)->middleware('auth:sanctum')->group(function () {
+
+    // [GET] /entries/en
+    Route::get('/entries/{lang}', 'index')->name('dictionary.index');
+
+    // [GET] /entries/en/{word}
+    Route::get('/entries/{lang}/{word}',  'show')->name('dictionary.show');
+});
